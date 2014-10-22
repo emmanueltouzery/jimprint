0ce8f3e00abd856db559eb0cbeadac989480f1e2
Emmanuel Touzery <etouzery@gmail.com>
2014-10-22 22:37:26 +0200

    convert the input images but it seems corrupted for now

--- src/jimprint/core.clj
+++ src/jimprint/core.clj
@@ -2,11 +2,22 @@
   (:require [clojurefx.core :refer :all])
   (:require [jimprint.javafx-helpers :refer :all])
   (:require [jimprint.settings :refer :all])
+  (import javafx.scene.image.Image)
+  (import javafx.embed.swing.SwingFXUtils)
+  (import java.io.File)
+  (import javax.imageio.ImageIO)
   (:gen-class))
 
+(defn convert-file [file]
+  (def img (Image. (.toString (.toURI file))))
+  (def outputFile (File. "/home/emmanuel/out.jpg"))
+  (ImageIO/write (SwingFXUtils/fromFXImage img nil) "jpeg" outputFile)
+  (println "OK"))
+
 (defn files-picked [files]
-  (println files)
-  )
+  ; calculate the output folder then...
+  ;(println files)
+  (apply convert-file files))
 
 (defn init-drop-target [froot]
     (def droptarget (.lookup froot "#droptarget"))
@@ -28,8 +39,7 @@
             (init-toolbar froot)
             (println "Hello, World!"))))
 
-(defn -main
-  [& args]
+(defn -main [& args]
   (show-window "jimprint.fxml" init-main-window))
  
  ; (.setScene stage (javafx.scene.Scene. froot 800 600))
