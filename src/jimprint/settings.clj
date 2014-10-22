5b8473a8e556d6a539b9bb9b4db397efb89b2803
Emmanuel Touzery <etouzery@gmail.com>
2014-10-22 19:37:01 +0200

    start work on preview

--- src/jimprint/settings.clj
+++ src/jimprint/settings.clj
@@ -5,9 +5,15 @@
 
 (defrecord TextStyle [style-id text-stroke text-fill stroke-height-ratio])
 
+(defn draw-preview [canvas]
+  (doto (.getGraphicsContext2D canvas)
+    (.setFill Color/BLUE)
+    (.fillRect 75 75 100 100)))
+
 (defn init-settings [froot]
   (def cur-style (atom (->TextStyle 1 (Color/rgb 255 0 0) (Color/rgb 0 255 0) 30)))
   (bind-property cur-style [:stroke-height-ratio] (.valueProperty (.lookup froot "#sizeslider")))
+  (draw-preview (.lookup froot "#previewcanvas"))
   (doto (.lookup froot "#okbutton")
     (.setOnMouseClicked (handle-action #(println @cur-style)))))
 
